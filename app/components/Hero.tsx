3bd38e0ef6410526213b21641a17c36d8f16a31c
juli sanz <jjulisanz@gmail.com>
2024-06-27 15:15:23 -0300

    hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import Button from "./Button";
+import Image from "next/image";
+
+const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    videoRef.current?.load();
+    videoRef.current?.play();
+  }, []);
+  return (
+    <section className="relative min-h-[calc(100vh-50px)] h-[calc(100vh-50px)] w-full flex flex-col items-center justify-center overflow-hidden">
+      {/* <div className="absolute inset-0  z-0 ">
+        <Image
+          src="/entrada.jpeg"
+          alt=""
+          fill
+          className="object-cover object-center"
+        />
+      </div> */}
+      <div className="blur absolute top-0 left-0 w-full h-full brightness-[.3]">
+        <video
+          className="w-full h-full object-cover object-center "
+          ref={videoRef}
+          loop
+          controls={false}
+          muted
+        >
+          <source src="/video.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <h1 className="text-9xl font-bold text-blanco font-serif z-10 row-span-1">
+        SAVORY
+      </h1>
+
+      <Button classNameProps="" />
+    </section>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,9 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
-import Button from "./components/Button";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative min-h-[calc(100vh-50px)] mt-[50px] grid grid-flow-col grid-rows-4 justify-center">
-      <div className="absolute inset-0 bg-cover bg-center bg-[url('/entrada.jpeg')] blur z-0 brightness-[.3]"></div>
-
-      {/* <div className="bg-gradient-to-b from-transparent from-10% to-marron-clarito to-10% w-28 z-10 " /> */}
-
-      {/* <div className="flex flex-col gap-2 bg-gradient-to-b from-transparent from-25% to-marron-clarito to-25%">
-          <h1 className="text-9xl font-bold text-blanco font-serif ">SAVORY</h1>
-          <h2 className="text-5xl text-marron">HELADERIA Y CAFETERIA</h2>
-        </div> */}
-      <h1 className="text-9xl font-bold text-blanco font-serif z-10 row-span-1">
-        SAVORY
-      </h1>
-      {/* <div className="flex flex-row z-10 w-full row-start-3 row-span-2">
-        <div className="bg-marron-clarito w-28 z-10 " />
-        <div className="w-full flex flex-col bg-slate-600">
-          <div className="w-full h-20 bg-marron-clarito" />
-          
-        </div>
-        <div className="bg-marron-clarito w-28 z-10 " />
-      </div> */}
-      <div className="flex flex-row items-end z-10 w-full bg-blanco row-start-3 row-span-2 ">
-        <div className="bg-marron-clarito p-24 mx-auto">
-          <Button classNameProps="" />
-        </div>
-      </div>
-
-      {/* <div className="bg-gradient-to-b from-transparent from-10% to-marron-clarito to-10% w-28 z-10 " /> */}
+    <main className="relative min-h-screen w-full flex flex-col pt-[50px]">
+      <Hero/>
     </main>
   );
 }
