33e3f9ad5ce941039058af26f3fa3cffb8cd474a
juli sanz <jjulisanz@gmail.com>
2024-09-06 16:02:04 -0300

    cart stopPropagation

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -15,7 +15,7 @@ import { MinusIcon } from "@/assets/icons/MinusIcon";
 import { shallow } from "zustand/shallow";
 import { cn } from "@/utils/cn";
 
-const Cart = ({classNameProp} : {classNameProp: string}) => {
+const Cart = ({ classNameProp }: { classNameProp: string }) => {
   const { cart, isCartOpen } = useCartStore(
     (state) => ({
       cart: state.cart,
@@ -60,6 +60,7 @@ const Cart = ({classNameProp} : {classNameProp: string}) => {
             exit={{ x: 0, opacity: 0 }}
             transition={{ ease: "easeInOut", duration: 0.5 }}
             className="bg-blanco-oscuro/90 w-full sm:w-1/3 max-h-screen p-6 overflow-auto overscroll-contain flex flex-col z-10 "
+            onClick={(e) => e.stopPropagation()}
           >
             <div className="w-full flex justify-between mb-4">
               <h2 className="text-marron text-4xl font-bold">Carrito</h2>
@@ -76,49 +77,47 @@ const Cart = ({classNameProp} : {classNameProp: string}) => {
               </p>
             ) : (
               cart.map((item) => (
-                <>
-                  <div className="w-full pb-2 flex gap-2 border-b border-marron-clarito mb-2">
-                    <Image
-                      src={item.imageSrc}
-                      alt=""
-                      width={200}
-                      height={200}
-                      className="object-cover rounded w-20 h-20"
-                    />
-                    <div className="text-marron flex flex-col flex-grow justify-between">
-                      <h3 className="text-xl font-semibold">{item.title}</h3>
-                      <div className="flex gap-2 items-center">
-                        <MinusIcon
-                          width={32}
-                          height={32}
-                          className="text-marron hover:scale-95 transition ease-in duration-150 hover:opacity-70 active:scale-95 active:opacity-70"
-                          onClick={() => decreaseQuantity(item.id)}
-                        />
-                        <p className="text-2xl font-sans font-semibold">
-                          {item.quantity}
-                        </p>
+                <div key={item.id} className="w-full pb-2 flex gap-2 border-b border-marron-clarito mb-2">
+                  <Image
+                    src={item.imageSrc}
+                    alt=""
+                    width={200}
+                    height={200}
+                    className="object-cover rounded w-20 h-20"
+                  />
+                  <div className="text-marron flex flex-col flex-grow justify-between">
+                    <h3 className="text-xl font-semibold">{item.title}</h3>
+                    <div className="flex gap-2 items-center">
+                      <MinusIcon
+                        width={32}
+                        height={32}
+                        className="text-marron hover:scale-95 transition ease-in duration-150 hover:opacity-70 active:scale-95 active:opacity-70"
+                        onClick={() => decreaseQuantity(item.id)}
+                      />
+                      <p className="text-2xl font-sans font-semibold">
+                        {item.quantity}
+                      </p>
 
-                        <PlusIcon
-                          width={32}
-                          height={32}
-                          className="text-marron hover:scale-95 transition ease-in duration-150 hover:opacity-70 active:scale-95 active:opacity-70"
-                          onClick={() => addToCart(item)}
-                        />
-                      </div>
-                    </div>
-                    <div className="text-marron flex flex-col justify-between col-span-1">
-                      <span className="font-semibold text-2xl">
-                        ${calculateIndividualPrice(item.price, item.quantity)}
-                      </span>
-
-                      <TrashIcon
-                        width={24}
-                        className="cursor-pointer text-red-500 hover:scale-95 transition ease-in duration-150 hover:opacity-70 active:scale-95 active:opacity-70"
-                        onClick={() => removeFromCart(item.id)}
+                      <PlusIcon
+                        width={32}
+                        height={32}
+                        className="text-marron hover:scale-95 transition ease-in duration-150 hover:opacity-70 active:scale-95 active:opacity-70"
+                        onClick={() => addToCart(item)}
                       />
                     </div>
                   </div>
-                </>
+                  <div className="text-marron flex flex-col justify-between col-span-1">
+                    <span className="font-semibold text-2xl">
+                      ${calculateIndividualPrice(item.price, item.quantity)}
+                    </span>
+
+                    <TrashIcon
+                      width={24}
+                      className="cursor-pointer text-red-500 hover:scale-95 transition ease-in duration-150 hover:opacity-70 active:scale-95 active:opacity-70"
+                      onClick={() => removeFromCart(item.id)}
+                    />
+                  </div>
+                </div>
               ))
             )}
             <div className="w-full text-marron flex justify-between">
